e6cdc6e58afb988e8f81dfcf6cb6258168d5689b
Jonathan Rupp <jrupp@rightpoint.com>
2024-12-14 17:35:16 -0600

    better prep for days

--- src/app/day0/component.tsx
+++ src/app/day0/component.tsx
@@ -3,9 +3,19 @@ import { tail, take, takeRight, zip } from 'lodash';
 
 export function Part1({ input }: { input: string }) {
   const parsed = input.split('\r\n').map((i) => i.split(/\s/).filter(Boolean).map(Number));
-  return <JsonList>{parsed}</JsonList>;
+  return (
+    <>
+      <Json>{'a'}</Json>
+      <JsonList>{parsed}</JsonList>
+    </>
+  );
 }
 export function Part2({ input }: { input: string }) {
   const parsed = input.split('\r\n').map((i) => i.split(/\s/).filter(Boolean).map(Number));
-  return <JsonList>{parsed}</JsonList>;
+  return (
+    <>
+      <Json>{'a'}</Json>
+      <JsonList>{parsed}</JsonList>
+    </>
+  );
 }
