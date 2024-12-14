2bbeaf6c5b9016b7f81bb726e2f3f60df2556190
Jonathan Rupp <jrupp@rightpoint.com>
2024-12-13 18:43:24 -0600

    day1

--- src/app/day1/component.tsx
+++ src/app/day1/component.tsx
@@ -1,10 +1,21 @@
-
-export function Part1({ input }: {input: string}) {
-    return <div>{input}</div>
-
+export function Part1({ input }: { input: string }) {
+  const parsed = input.split('\n').map((i) => i.split(/\s/).filter(Boolean).map(Number));
+  const list1 = parsed.map((i) => i[0]).sort();
+  const list2 = parsed.map((i) => i[1]).sort();
+  const distances = list1.map((i, idx) => Math.abs(i - list2[idx]));
+  const sum = distances.reduce((acc, curr) => acc + curr, 0);
+  return <div>{sum}</div>;
 }
 
-export function Part2({ input }: {input: string}) {
-    return <div>{input}</div>
-
+export function Part2({ input }: { input: string }) {
+  const parsed = input.split('\n').map((i) => i.split(/\s/).filter(Boolean).map(Number));
+  const list1 = parsed.map((i) => i[0]).sort();
+  const list2 = parsed.map((i) => i[1]).sort();
+  const freqs = list2.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {} as Record<number, number>);
+  const values = list1.map((i) => i * freqs[i] || 0);
+  const sum = values.reduce((acc, curr) => acc + curr, 0);
+  return <div>{sum}</div>;
 }
--- src/app/day1/page.tsx
+++ src/app/day1/page.tsx
@@ -1,13 +1,15 @@
-import { readFile } from "fs/promises";
-import { Part1, Part2 } from "./component";
+import input from './input.txt';
+import { Part1, Part2 } from './component';
 
 export default async function () {
-  const input = await readFile("input.txt", "utf8");
+  //   const input = await readFile("./input.txt", "utf8");
 
   return (
     <>
+      <h3>Part 1</h3>
       <Part1 {...{ input }} />
-      {/* <Part2 {...{input}} /> */}
+      <h3>Part 2</h3>
+      <Part2 {...{ input }} />
     </>
   );
 }
